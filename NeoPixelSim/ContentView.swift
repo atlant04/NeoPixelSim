cc6295f79120b571f961fbdbdd808ef8248c21fc
Maksim Tochilkin <mtoch777@gmail.com>
2022-04-17 23:17:24 -0400

    Initial Commit

---
+++ NeoPixelSim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeoPixelSim
+//
+//  Created by Maksim Tochilkin on 4/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeoPixelSim/NeoPixelSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoPixelSimApp.swift
+//  NeoPixelSim
+//
+//  Created by Maksim Tochilkin on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct NeoPixelSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
